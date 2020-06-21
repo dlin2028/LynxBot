de9608a8fc31b10aae8a947ccb220456f3dee3fb
David Lin <dlin2028@gmail.com>
2020-06-21 03:04:40 -0700

    Initial

---
+++ HTMS/Modules/InviteCounter.cs
@@ -0,0 +1,80 @@
+﻿using Discord;
+using Discord.Commands;
+using F23.StringSimilarity;
+using Newtonsoft.Json.Linq;
+using org.mariuszgromada.math.mxparser;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace HTMS.Modules
+{
+    public class InviteCounter : ModuleBase<SocketCommandContext>
+    {
+        private static Dictionary<IUser, IInvite> invites = null;
+
+        [Command("createinvite")]
+        public async Task createInvite()
+        {
+            if (invites == null)
+            {
+                try
+                {
+                    invites = JsonSerializer.Deserialize<Dictionary<IUser, IInvite>>(File.ReadAllText("invites"));
+                }
+                catch
+                {
+                    invites = new Dictionary<IUser, IInvite>();
+                }
+            }
+
+            IUser user = Context.User as IUser;
+            ITextChannel chnl = Context.Client.GetChannel(Context.Channel.Id) as ITextChannel;
+            IInvite invite = await chnl.CreateInviteAsync(maxAge: null);
+            invites.Add(user, invite);
+
+            var jsonString = JsonSerializer.Serialize(invites);
+            File.WriteAllText("invites", jsonString);
+
+            await Context.Channel.SendMessageAsync(invite.Url);
+        }
+
+
+        [Command("invites")]
+        public async Task getInvites()
+        {
+            if (invites == null)
+            {
+                try
+                {
+                    invites = JsonSerializer.Deserialize<Dictionary<IUser, IInvite>>(File.ReadAllText("invites"));
+                }
+                catch
+                {
+                    invites = new Dictionary<IUser, IInvite>();
+                }
+            }
+            if(invites.ContainsKey(Context.User as IUser))
+            {
+                IInvite invite = invites[Context.User as IUser];
+                ITextChannel chnl = Context.Client.GetChannel(Context.Channel.Id) as ITextChannel;
+                var thing = await chnl.GetInvitesAsync();
+                var realInvite = thing.Where(x => x.Id == invite.Id).FirstOrDefault();
+                await Context.Channel.SendMessageAsync(realInvite.Uses.ToString());
+            }
+            else
+            {
+                await Context.Channel.SendMessageAsync("0");
+            }
+        }
+    }
+}
---
+++ HTMS/Program.cs
@@ -0,0 +1,39 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+
+namespace HTMS
+{
+    class Program
+    {
+        static void Main(string[] args)
+            => new Program().StartAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient client;
+
+        private async Task StartAsync()
+        {
+            client = new DiscordSocketClient();
+
+            new CommandHandler(client);
+
+            client.Log += Log;
+            client.MessageReceived += MsgRecieved;
+
+            await client.LoginAsync(TokenType.Bot, "NzI0MTgyODQyNjQ0NDk2NDY0.Xu8pug.8-gWy76J6aAUBp3x6wmj3TiSWoQ");
+            await client.StartAsync();
+            await Task.Delay(-1);
+        }
+        private Task MsgRecieved(SocketMessage message)
+        {
+            Console.WriteLine(message.ToString());
+            return Task.CompletedTask;
+        }
+        private Task Log(LogMessage message)
+        {
+            Console.WriteLine(message.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
